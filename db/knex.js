7804a09fbce16e7d73223193a1bf2873e02661f3
javier.linan <javier.linan@sandavteam.com>
2023-05-17 13:30:59 +0200

    Minor changes

--- db/knex.js
+++ db/knex.js
@@ -7,5 +7,4 @@ const connection = knex({
     }
 });
 
-
 module.exports = connection;
--- express.js
+++ express.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 1200; // Set your preferred port number
+const port = 1200;
+const pagesDir = __dirname + '/pages/';
+
+app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('static'));
 
-// Using static automatically allows the
-app.use(express.static(__dirname + 'public/'));
+app.get('', (req, res) => {
+    res.sendFile(`${pagesDir}`);
+});
+
+const booksRouter = require('./routes/books');
+const authorsRouter = require('./routes/authors');
+app.use('/books', booksRouter);
+app.use('/authors', authorsRouter);
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}\nServer's url: http://localhost:${port}/`);
+    console.log(`Server is running on port ${port}\nServer's url: http://localhost:${port}/`);
 });
