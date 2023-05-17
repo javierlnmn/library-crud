b05b968f8493b1ccf0b5826f22f128cd7d82652f
javier.linan <javier.linan@sandavteam.com>
2023-05-17 13:48:09 +0200

    Minor corrections

--- routes/authors.js
+++ routes/authors.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const queries = require('./../db/queries');
 
 router.get('/', async (req, res) => {
     const authors = await queries.getAllAuthors();
