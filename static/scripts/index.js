8850218aad85f6932864fe5157f8e339ea48cbd5
javierlnmn <jlm03123@gmail.com>
2023-05-19 15:53:31 +0200

    Head html block and JS script

---
+++ static/scripts/index.js
@@ -0,0 +1,6 @@
+$(".input").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $(".table tr").filter(function () {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+});
